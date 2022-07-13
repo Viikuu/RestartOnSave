377b39c9c1006a3e83eb250a9dce1d1ff002e5fa
Viikuu <viiktorw77@gmail.com>
2022-07-13 12:37:27 +0200

    Code refactor
    Add restart option after error in openned child program
    Add fswachers close funtionality whenever child program is killed
    Rename funcions
    Add more readability to log messages
    Chang import from fs to watch

--- app.mjs
+++ app.mjs
@@ -1,11 +1,11 @@
-import fs from 'node:fs';
+import {watch} from 'node:fs';
 import {spawn} from 'node:child_process';
 import process from 'node:process';
 import {directory} from './src/foldering.mjs';
 
 const filedir = process.argv[2];
 
-function killchild(child) {
+function killChild(child) {
 	child.once('exit', () => {
 		clearInterval(killTimeout);
 	});
@@ -21,28 +21,42 @@ async function autoRestart() {
 		? [filedir]
 		: await directory(process.argv[3]);
 
-	await console.log('---Starting process...');
+	await console.log('--- Starting process...');
 	const child = await spawn(process.execPath, [filedir], {stdio: 'inherit'});
 
-	process.addListener('exit', () => {
+	process.once('exit', () => {
 		if (!child.killed) {
-			killchild(child);
+			killChild(child);
 		}
 	});
 	child.on('close', async code => {
 		if (code === null) {
+			console.log('--- Killing process...\n');
+			for (const watcher of watchers) {
+				watcher.close();
+			}
+
 			await autoRestart(filedir);
+		} else {
+			console.log('--- Error occured\n--- Restarting program in 5sec...\n');
+			setTimeout(async () => {
+				for (const watcher of watchers) {
+					watcher.close();
+				}
+
+				await autoRestart(filedir);
+			}, 5000);
 		}
 	});
+	const watchers = [];
 	for (const file of projectdir) {
-		fs.watch(file, {
+		watchers.push(watch(file, {
 			persistent: false,
 		}, () => {
 			if (!child.killed) {
-				console.log('---Killing process...\n');
-				killchild(child);
+				killChild(child);
 			}
-		});
+		}));
 	}
 }
 
